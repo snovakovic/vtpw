cc2ab2b674d30b7071550cc9e99b3413b1c59ae8
snovakovic <stefan.novakovich@gmail.com>
2019-10-07 17:22:08 +0200

    Add .vscode file

--- out/extension.js
+++ out/extension.js
@@ -9,44 +9,27 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-var fs = require('fs');
-const last = (arr) => arr[arr.length - 1];
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const fs = require("fs");
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vetur-typescript-performance-workraound" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.vtpw', () => __awaiter(this, void 0, void 0, function* () {
         const { activeTextEditor } = vscode.window;
         const { workspace } = vscode;
         if (!activeTextEditor || !workspace || !workspace.workspaceFolders) {
-            vscode.window.showWarningMessage(`No active .vue files`);
+            vscode.window.showWarningMessage(`Works only for .vue files`);
             return;
         }
         const activeFileLocation = activeTextEditor.document.fileName;
         if (!activeFileLocation.endsWith('.vue')) {
-            vscode.window.showWarningMessage(`Works only for .vue single file components`);
+            vscode.window.showWarningMessage(`Works only for .vue files`);
             return;
         }
-        const activeFileName = last(activeFileLocation.split('/'));
         vscode.window.showInformationMessage(`active file => ${activeFileLocation}`);
-        const activeFileContent = activeTextEditor.document.getText();
-        // TODO: rootPath is depricated (can have multiple workspace)
-        // Maybe best to pick based on activeFileName??
-        var dir = `${workspace.rootPath}/vtpw_temp`;
-        // TODO convert to async asn use fsStat (as exists is depricated)
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir);
-        }
-        // TODO change to async version
-        const tsFile = `${dir}/${activeFileName.replace('vue', 'ts')}`;
+        // const activeFileContent = activeTextEditor.document.getText();
+        // Create new .vtpw.ts file next to .vue file
+        // Have to be in same location in order for all imports to work as expected
+        const tsFile = `${activeFileLocation.replace('.vue', '.vtpw.ts')}`;
+        // TODO: check to async version
         fs.copyFileSync(activeFileLocation, tsFile);
         const tsFileDocument = yield workspace.openTextDocument(tsFile);
         vscode.window.showTextDocument(tsFileDocument);
---
+++ out/utils.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.last = (arr) => arr[arr.length - 1];
+//# sourceMappingURL=utils.js.map
