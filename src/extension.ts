b71493c76ba1dbe1854fe512768aca885efe508a
snovakovic <stefan.novakovich@gmail.com>
2019-10-08 22:05:38 +0200

    Write file in async

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,13 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-import { commentOutVueComponentTags } from './utils';
+import {
+	commentOutVueComponentTags,
+	revertCommentingOutOfVueTags,
+	removeFileIfExists,
+	promisify,
+} from './utils';
+
+const writeFile = promisify(fs.writeFile);
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.vtpw', async () => {
@@ -24,16 +31,31 @@ export function activate(context: vscode.ExtensionContext) {
 		// Create new .vtpw.ts file next to .vue file
 		// Have to be in same location in order for all imports to work as expected
 		const tsFileLocation = `${vueFileLocation.replace('.vue', '.vtpw.ts')}`;
-
-		// TODO: change to async version
-		fs.writeFileSync(tsFileLocation, validTsFileContent);
+		await writeFile(tsFileLocation, validTsFileContent);
 
 		// Bring focus to newly created ts file (TODO: cursor position??)
-		const tsFileDocument = await workspace.openTextDocument(tsFileLocation);
-		vscode.window.showTextDocument(tsFileDocument);
+		// const tsFileDocument = await workspace.openTextDocument(tsFileLocation);
+		const tsFileUri = vscode.Uri.file(tsFileLocation);
+		vscode.window.showTextDocument(tsFileUri);
 
+		// On saving shadow ts file sync changes back to vue file
 		workspace.onDidSaveTextDocument((doc) => {
-			console.log('text documetn saved');
+			if(tsFileLocation === doc.fileName) {
+				const tsFileContent = doc.getText();
+				const validVueFileContent = revertCommentingOutOfVueTags(tsFileContent);
+
+				writeFile(vueFileLocation, validVueFileContent);
+			}
+		});
+
+		// On closing shadow ts file remove it from disk
+		// TODO: not triggered when closing new file???
+		// Maybe on other change check => vscode.window.visibleTextEditors and close if not in list
+		// Keeping destroyed flag or something
+		workspace.onDidCloseTextDocument((doc) => {
+			if(tsFileLocation === doc.fileName) {
+				removeFileIfExists(tsFileLocation);
+			}
 		});
 	});
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,31 @@
+import * as fs from 'fs';
+
 export const last = <T>(arr:T[]) => arr[arr.length - 1];
+
+export function removeFileIfExists(location:string) {
+  fs.stat(location, (err) => {
+    if (!err) {
+      fs.unlink(location, () => {});
+    }
+  });
+}
+
+// TODO: js-flock??
+export function promisify(fn:any) {
+  return function(this:any, ...args:any[]) {
+    return new Promise((resolve, reject) => {
+      args.push((err:any, ...result:any[]) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(result[0]);
+      });
+
+      fn.apply(this, args);
+    });
+  };
+}
 
 export function commentOutVueComponentTags(vueFile:string) {
   // TODO: improve with regex to be more flexible
